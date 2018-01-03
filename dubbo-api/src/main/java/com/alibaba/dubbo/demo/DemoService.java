feb5012d7e2c906f1bc4027bf510a8e2d3869c2b
Cheung <550690513@qq.com>
2018-01-03 11:09:50 +0800

    Add files via upload

---
+++ dubbo-api/src/main/java/com/alibaba/dubbo/demo/DemoService.java
@@ -0,0 +1,10 @@
+package com.alibaba.dubbo.demo;
+
+/**
+ * Created by Cheung on 2017/12/22
+ */
+public interface DemoService {
+
+	String sayHello(String name);
+
+}
---
+++ dubbo-consumer2/src/main/java/com/alibaba/dubbo/consumer/Consumer.java
@@ -0,0 +1,27 @@
+package com.alibaba.dubbo.consumer;
+
+import com.alibaba.dubbo.demo.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Cheung on 2017/12/22
+ */
+public class Consumer {
+
+	private static final String NAME = "consumer2";
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
+		context.start();
+
+		System.out.println("服务消费方已启动---" + NAME);
+
+		DemoService demoService = context.getBean(DemoService.class);// 获取远程服务代理
+		String result = demoService.sayHello(NAME);// 远程调用方法
+
+		System.out.println(result);// 打印结果
+
+	}
+
+}
